b18aaccc7209a1ab49fef63ac3996da528d741f8
Zodar22 <atoledob04@gmail.com>
2023-03-28 00:33:25 -0600

    integracion canActivate en rutas modulo PEC

--- src/app/dor/dor-routing.module.ts
+++ src/app/dor/dor-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DorComponent } from './container/dor.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { MsalInterceptor } from '@azure/msal-angular';
+import { MsalGuard, MsalInterceptor } from '@azure/msal-angular';
 import { DorCapturaComponent } from './components/dor-captura/dor-captura.component';
 import { DorObjetivosComponent } from './components/dor-objetivos/dor-objetivos.component';
 import { DorEvaluacionComponent } from './components/dor-evaluacion/dor-evaluacion.component';
 
 const routes: Routes = [
   { path: '', component: DorComponent },
-  { path: 'captura', component: DorCapturaComponent },
-  { path: 'evaluacion', component: DorEvaluacionComponent },
-  { path: 'objetivos', component: DorObjetivosComponent }
+  { path: 'captura', component: DorCapturaComponent, canActivate: [MsalGuard] },
+  { path: 'evaluacion', component: DorEvaluacionComponent, canActivate: [MsalGuard] },
+  { path: 'objetivos', component: DorObjetivosComponent, canActivate: [MsalGuard] }
 ];
 
 @NgModule({
